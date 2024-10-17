c7ab4703a4fdf2406425f99706252c981d9e42ac
Lingga Morthy <lingga@Linggas-MacBook-Air.local>
2024-10-17 23:06:18 +0800

    amend migration

--- database/migrations/2024_10_17_144137_create_urls_table.php
+++ database/migrations/2024_10_17_144137_create_urls_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
             $table->string('url_data', length: 2083);
+            $table->string('code', length: 5);
             $table->smallInteger('visit_count')->default(0);
             $table->timestamps();
         });
