dc79f3317cc0da3cd7608a6f5bbe966b694f5b5c
Lingga Morthy <lingga@Linggas-MacBook-Air.local>
2024-10-18 15:49:58 +0800

    add comments

--- app/Helpers/Utility.php
+++ app/Helpers/Utility.php
@@ -4,16 +4,22 @@ namespace App\Helpers;
 
 class Utility {
 
-  public static function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
+    public static function generateRandomString($length = 10): string 
+    {
+        /** 
+         * generate an alphanumeric string based
+         * on the specific length given
+        */
+        
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
 
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
     }
-
-    return $randomString;
-  }
 
 }
