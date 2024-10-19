b03a06a50efe98de3b9240e539ce24bab31fe18f
Lingga Morthy <lingga@Linggas-MacBook-Air.local>
2024-10-19 20:15:55 +0800

    update lock timeout

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -7,6 +7,8 @@ use Illuminate\View\View;
 use App\Services\UrlService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Cache\LockTimeoutException;
 
 class UrlController extends Controller
 {
@@ -27,14 +29,24 @@ class UrlController extends Controller
             'url_data' => 'required|url:https|max:2083',
         ]);
 
+        $lock = Cache::lock('generate-short-url', 10); // set 10 seconds lock
+
         try {
+
+            $lock->block(5); //wait 5 seconds if cant acquire lock 
+
             $record = $this->urlService->create($data);
             $short_url = url($record->code);
             return redirect()->back()->with('message', "Your short url: $short_url");
+        }catch (LockTimeoutException $e) {
+            Log::error('unable to obtain lock ' . $e->getMessage());
+            abort(500, $e->getMessage());
         } catch (\Exception $e) {
             Log::error('Error creating short url: ' . $e->getMessage());
             abort(500, $e->getMessage());
-        }
+        } finally {
+            $lock->release();
+        } 
     }
 
     public function loadUrl(string $code,Request $request): RedirectResponse
